b56b7aa2372fd529862d10f577fd8e2b303dd065
Priyansh Srivastava <spriyansh29@gmail.com>
2023-10-23 10:37:48 +0000

    Reviewed "annotate_sce".

--- R/annotate_sce.R
+++ R/annotate_sce.R
@@ -2,27 +2,31 @@
 #'
 #' @description
 #' `annotate_sce()` annotates a SingleCellExperiment class object with pseudotime
-#' and path information in its `cell.metadata` generated using `colData` from the \pkg{SingleCellExperiment} package.
+#' and path information in its `cell.metadata` generated using `colData` from the 
+#' \pkg{SingleCellExperiment} package.
 #'
 #' @param sce A SingleCellExperiment object to be annotated.
-#' @param pseudotime_colname Name of the column in `cell.metadata` generated using
-#' `colData` from the \pkg{SingleCellExperiment} package, storing information for Pseudotime.
-#' (Default is "Pseudotime")
+#' @param pseudotime_colname Name of the column in `cell.metadata`  storing 
+#' information for Pseudotime. It is generated using `colData` from the 
+#' \pkg{SingleCellExperiment} package. (Default is "Pseudotime").
 #' @param path_prefix Prefix used to annotate the paths. (Default is "Path").
 #' @param root_label Label used to annotate root cells. (Default is "root").
-#' @param path_colname Name of the column in `cell.metadata` generated using
-#' `colData` from the \pkg{SingleCellExperiment} package, storing information for Path.
-#' (Default is `path_prefix`)
-#' @param existing_pseudotime_colname The name of an existing pseudotime column to be replaced (if not NULL).
-#' @param existing_path_colname The name of an existing path column to be replaced (if not NULL).
-#' @param overwrite_labels Logical, should existing column names be overwritten if they exist? (default is TRUE).
+#' @param path_colname Name of the column in `cell.metadata` storing information 
+#' for Path. It is generated using `colData` from the \pkg{SingleCellExperiment} 
+#' package. (Default is `path_prefix`).
+#' @param existing_pseudotime_colname The name of an existing pseudotime column 
+#' to be replaced (if not NULL).
+#' @param existing_path_colname The name of an existing path column to be replaced 
+#' (if not NULL).
+#' @param overwrite_labels Logical, should existing column names be overwritten 
+#' if they already exist? (default is TRUE).
 #' @param verbose Print detailed output in the console. (Default is TRUE)
 #'
 #' @return A SingleCellExperiment object with updated cell metadata.
 #'
 #' @details Additional Details
 #'
-#' @seealso SingleCellExperiment class object, `colData` from the \pkg{SingleCellExperiment} package
+#' @seealso SingleCellExperiment class object, `colData` from the \pkg{SingleCellExperiment} package.
 #'
 #' @examples
 #' # Annotate a SingleCellExperiment object with pseudotime and path information
@@ -61,12 +65,12 @@ annotate_sce <- function(sce,
     # Check columns
     assert_that(
       all(existing_pseudotime_colname %in% colnames(cell.meta)),
-      msg = paste("'", existing_pseudotime_colname, "', doesn't exist in cell.metadata")
+      msg = paste("'", existing_pseudotime_colname, "', doesn't exist in cell.metadata.")
     )
     # Check columns
     assert_that(
       all(existing_path_colname %in% colnames(cell.meta)),
-      msg = paste("'", existing_path_colname, "', doesn't exist in cell.metadata")
+      msg = paste("'", existing_path_colname, "', doesn't exist in cell.metadata.")
     )
 
     # Override
