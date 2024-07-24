aba86cc5599eada6b692af3ca16b5b7b3946711c
spriyansh <spriyansh29@gmail.com>
2024-07-24 14:47:43 +0200

    - Remove individual data docs - Create single data doc

--- R/data_doc.R
+++ R/data_doc.R
@@ -70,3 +70,18 @@
 #' @author Priyansh Srivastava \email{spriyansh29@@gmail.com}
 #'
 "splat.sim"
+
+
+#' @title scMaSigPro Object with results
+#'
+#' @description
+#' A small scMaSigPro object created from simulated data. Please follow Vignette
+#' `scMaSigPro: Quick Start Guide` for more details.
+#'
+#' @usage
+#' # Loading
+#' data("scmp.ob", package = "scMaSigPro")
+#'
+#' @author Priyansh Srivastava \email{spriyansh29@@gmail.com}
+#'
+"scmp.ob"
